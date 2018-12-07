df96d4d7cc24cf44b576b9e2986457a3aba19fe4
Dan E <dan@ehrlichserver.com>
2018-12-06 23:10:11 -0600

    update

--- config/linux/ubuntu/ubuntu1804.sh
+++ config/linux/ubuntu/ubuntu1804.sh
@@ -1,40 +1,39 @@
 apt-get update
 apt-get upgrade -y
-#others
+apt-get install php7.2 php7.2-bcmath php7.2-bz2 php7.2-cgi php7.2-cli php7.2-common php7.2-curl php7.2-dba php7.2-enchant \
+php7.2-fpm php7.2-gd php7.2-gmp php7.2-imap php7.2-interbase php7.2-intl php7.2-json php7.2-ldap php7.2-mbstring \
+php7.2-mysql php7.2-odbc php7.2-opcache php7.2-pgsql php7.2-phpdbg php7.2-pspell php7.2-readline \ 
+php7.2-recode php7.2-snmp php7.2-soap php7.2-sqlite3 php7.2-tidy php7.2-xml php7.2-xsl php7.2-zip \ 
+php-redis php-igbinary php7.2-mysql perl tar curl nodejs npm mysql-client python python-pip \ 
+imagemagick libapache2-mod-php7.2 git composer vim -y
 
-if ! [ -x "$(command -v sudo)" ]
-    then 
+#if ! [ -x "$(command -v sudo)" ]
+    #then 
       apt-get install php7.2 php7.2-bcmath php7.2-bz2 php7.2-cgi php7.2-cli php7.2-common php7.2-curl php7.2-dba php7.2-enchant php7.2-fpm php7.2-gd php7.2-gmp php7.2-imap php7.2-interbase php7.2-intl php7.2-json php7.2-ldap php7.2-mbstring php7.2-mysql php7.2-odbc php7.2-opcache php7.2-pgsql php7.2-phpdbg php7.2-pspell php7.2-readline php7.2-recode php7.2-snmp php7.2-soap php7.2-sqlite3 php7.2-tidy php7.2-xml php7.2-xsl php7.2-zip php-redis php-igbinary php7.2-mysql perl tar curl nodejs npm mysql-client python python-pip imagemagick libapache2-mod-php7.2 git composer vim -y
-else	    
-  echo "hello"    	    
-fi
+#else	    
+  #echo "hello"    	    
+#fi
 
- git clone https://github.com/openemr/openemr.git --depth 1 
- rm -rf openemr/.git
- cd openemr 
- composer install 
- npm install --unsafe-perm 
- npm run build 
- composer global require phing/phing 
- composer global require phing/phing 
-  #/root/.composer/vendor/bin/phing vendor-clean \
-  #/root/.composer/vendor/bin/phing assets-clean \
- composer global remove phing/phing 
- composer dump-autoload -o 
- composer clearcache 
- npm cache clear --force 
- rm -fr node_modules 
- cd ../ 
- chmod 666 openemr/sites/default/sqlconf.php 
- chmod 666 openemr/interface/modules/zend_modules/config/application.config.php 
- chown -R www-data openemr/ 
- mv openemr /var/www/ 
-
- #php.ini
- #openemr.conf
- #run_opneemrsh
-# remove default site in sites-enabled
-# remove /var/www/html*
+git clone https://github.com/openemr/openemr.git --depth 1 
+rm -rf openemr/.git
+cd openemr 
+composer install 
+npm install --unsafe-perm 
+npm run build 
+composer global require phing/phing 
+composer global require phing/phing 
+#/root/.composer/vendor/bin/phing vendor-clean \
+#/root/.composer/vendor/bin/phing assets-clean \
+composer global remove phing/phing 
+composer dump-autoload -o 
+composer clearcache 
+npm cache clear --force 
+rm -fr node_modules 
+cd ../ 
+chmod 666 openemr/sites/default/sqlconf.php 
+chmod 666 openemr/interface/modules/zend_modules/config/application.config.php 
+chown -R www-data openemr/ 
+mv openemr /var/www/ 
 
 ### SSL
 git clone https://github.com/letsencrypt/letsencrypt /opt/certbot 
@@ -52,6 +51,7 @@ openssl req -x509 -newkey rsa:4096 \
 #ln -s /etc/ssl/private/selfsigned.key.pem /etc/ssl/private/webserver.key.pem
 
 ### Config Files
+rm -rf /var/www/html
 rm -f /etc/apache2/apache2.conf
 rm -f /etc/apache2/conf-enabled/security.conf
 rm -f /etc/apache2/sites-enabled/000-default.conf
@@ -67,6 +67,7 @@ ln -s /etc/apache2/mods-available/rewrite.conf /etc/apache2/mods-enabled/rewrite
 
 ### More File Permissions
 echo "Default file permissions and ownership set, allowing writing to specific directories"
+cd /var/www/openemr
 chmod 700 run_openemr.sh
 # Set file and directory permissions
 chmod 600 interface/modules/zend_modules/config/application.config.php
@@ -79,14 +80,14 @@ find interface/main/calendar/modules/PostCalendar/pntemplates/compiled -type d -
 find gacl/admin/templates_c -type d -print0 | xargs -0 chmod 700
 
 ### Script Removal
-echo "Removing remaining setup scripts"
+#echo "Removing remaining setup scripts"
 #remove all setup scripts
-rm -f admin.php
-rm -f acl_setup.php
-rm -f acl_upgrade.php
-rm -f setup.php
-rm -f sql_patch.php
-rm -f sql_upgrade.php
-rm -f ippf_upgrade.php
-rm -f gacl/setup.php
-echo "Setup scripts removed, we should be ready to go now!"
+#rm -f admin.php
+#rm -f acl_setup.php
+#rm -f acl_upgrade.php
+#rm -f setup.php
+#rm -f sql_patch.php
+#rm -f sql_upgrade.php
+#rm -f ippf_upgrade.php
+#rm -f gacl/setup.php
+#echo "Setup scripts removed, we should be ready to go now!"
